f47a8630c9349603ef14008be9abf38f19ec0213
Viznem <thinhtruong3344@gmail.com>
2023-11-11 09:02:49 +0700

    Add Front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './layouts/Main';
+import Dashboard from './components/Dashboard';
+import Register from './components/Register';
+import UserLogin from './components/UserLogin';
+import { PrivateRoutes } from './layouts/PrivateRoutes';
+import { PublicRoutes } from './layouts/PublicRoutes';
+import { Layout } from './layouts/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route element={<Main/>}>
+          <Route element={<PrivateRoutes/>}>
+            <Route path='/' element={<Dashboard />}/>
+          </Route>
+        </Route>
+        <Route element={<PublicRoutes/>}>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/login' element={<UserLogin />}/>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
